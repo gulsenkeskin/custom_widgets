6b03fea6ce20859ed6e095d205431e19e35ab723
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-26 16:13:02 +0300

    Update popup_dialog.dart

--- lib/src/pages/popup_dialog.dart
+++ lib/src/pages/popup_dialog.dart
@@ -5,14 +5,30 @@ class PopupDialogPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title:const Text("Popup Dialog"),
-    ),
-    body: Center(child: ElevatedButton(
-      child:const Text('Open Dialog'),
-      onPressed: (){
-
-      },
-    ),),
-  );
+        appBar: AppBar(
+          title: const Text("Popup Dialog"),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            child: const Text('Open Dialog'),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                        actions: [
+                          TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text('CANCEL')),
+                          TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text('OK'))
+                        ],
+                        title: const Text('My Title'),
+                        content: const Text(
+                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                      ));
+            },
+          ),
+        ),
+      );
 }
