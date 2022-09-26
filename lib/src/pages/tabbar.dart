90d587c70bff030733412c4d24343d8d3fe25608
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-26 22:06:30 +0300

    Update tabbar.dart

--- lib/src/pages/tabbar.dart
+++ lib/src/pages/tabbar.dart
@@ -4,9 +4,34 @@ class TabBarPage extends StatelessWidget {
   const TabBarPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-
-    );
-  }
+  Widget build(BuildContext context) => DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('TabBar'),
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: 'Tab 1'),
+                Tab(
+                  text: 'Tab 2',
+                ),
+                Tab(text: 'Tab 3')
+              ],
+            ),
+          ),
+          body: const TabBarView(
+            children: [
+              Center(
+                child: Text('Page 1'),
+              ),
+              Center(
+                child: Text('Page 2'),
+              ),
+              Center(
+                child: Text('Page 3'),
+              ),
+            ],
+          ),
+        ),
+      );
 }
