239a30d71858fd72eceb1e2aa435783512746c39
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-26 21:10:00 +0300

    listview onPageChanged

--- lib/src/pages/page_view.dart
+++ lib/src/pages/page_view.dart
@@ -4,10 +4,28 @@ class PageViewPage extends StatelessWidget {
   const PageViewPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Expansion Tile Widget"),
+        ),
+        body: PageView(
+          scrollDirection: Axis.vertical,
+          onPageChanged: (index){
+            print('Page ${index+1}');
+          },
+          children: [
+            customContainer(Colors.red, "Page 1"),
+            customContainer(Colors.indigo, "Page 2"),
+            customContainer(Colors.green, "Page 3"),
+          ],
+        ),
+      );
+
+  Container customContainer(Color color, String text) {
     return Container(
-
-
-    );
+        color: color,
+        child: Center(
+          child: Text(text),
+        ));
   }
 }
