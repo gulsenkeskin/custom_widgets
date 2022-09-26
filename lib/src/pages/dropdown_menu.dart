9283c149b6b306cee6d822a782d32efa470004b1
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-26 16:15:24 +0300

    Update dropdown_menu.dart

--- lib/src/pages/dropdown_menu.dart
+++ lib/src/pages/dropdown_menu.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
-class DropdownMenuPage extends StatelessWidget {
+class DropdownMenuPage extends StatefulWidget {
   const DropdownMenuPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
+  State<DropdownMenuPage> createState() => _DropdownMenuPageState();
+}
 
-    );
-  }
+class _DropdownMenuPageState extends State<DropdownMenuPage> {
+
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Expansion Tile Widget"),
+        ),
+        body: SizedBox(),
+      );
 }
