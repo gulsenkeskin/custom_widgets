0abe6bc1329646d7e3c92954ba016864ef9849dc
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-26 15:26:59 +0300

    Update route_list.dart

--- lib/constants/route_list.dart
+++ lib/constants/route_list.dart
@@ -1,8 +1,10 @@
+import 'package:custom_widgets/src/pages/bottom_sheet.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 List<Map<String, Object>> routeList=[
   {
-    "name":"",
-    "route": SizedBox()
+    "name":"Bottom Sheet",
+    "route": BottomSheetPage()
   }
 ];
