6e7541adc783c277166100ebc039bd30bbb6dc10
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-26 15:20:18 +0300

    Update home.dart

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:custom_widgets/constants/route_list.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +6,38 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Custom Widgets"),
+      ),
+      body: ListView.builder(
+          itemCount: routeList.length,
+          itemBuilder: (context, index) {
+            final name = routeList[index]["name"] as String;
+            final route = routeList[index]["route"] as Widget;
+
+            return ListTile(
+              title: customCard(index, name),
+              onTap: () => Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => route)),
+            );
+          }),
+    );
+  }
+
+  Card customCard(int index, String item) {
+    return Card(
+      color: (index % 2 == 0) ? Colors.blueGrey[100] : Colors.blueGrey[50],
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Text(
+          item,
+          style: const TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 15,
+          ),
+        ),
+      ),
+    );
   }
 }
