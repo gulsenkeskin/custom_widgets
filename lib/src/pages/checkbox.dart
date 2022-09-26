92242148c363a8102b7330c067ce5ad07c9638af
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-26 15:57:54 +0300

    Update checkbox.dart

--- lib/src/pages/checkbox.dart
+++ lib/src/pages/checkbox.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
-class CheckBoxPage extends StatelessWidget {
+class CheckBoxPage extends StatefulWidget {
   const CheckBoxPage({Key? key}) : super(key: key);
 
   @override
+  State<CheckBoxPage> createState() => _CheckBoxPageState();
+}
+
+class _CheckBoxPageState extends State<CheckBoxPage> {
+  bool isChecked = false;
+  @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title:const Text("Expansion Tile Widget"),
-    ),
-    body: SizedBox(),
-  );
+        appBar: AppBar(
+          title: const Text("Checkbox Widget"),
+        ),
+        body: Center(
+          child: Checkbox(
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value!;
+              });
+            },
+          ),
+        ),
+      );
 }
