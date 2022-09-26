dbcd4342adca047fe4184caff3d78900c54bfc83
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-26 23:03:48 +0300

    confetti :heartbeat:

--- lib/src/pages/animation_pages/confetti.dart
+++ lib/src/pages/animation_pages/confetti.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 
 class ConfettiPage extends StatefulWidget {
@@ -8,10 +11,199 @@ class ConfettiPage extends StatefulWidget {
 }
 
 class _ConfettiPageState extends State<ConfettiPage> {
+  late ConfettiController _controllerCenter;
+  late ConfettiController _controllerCenterRight;
+  late ConfettiController _controllerCenterLeft;
+  late ConfettiController _controllerTopCenter;
+  late ConfettiController _controllerBottomCenter;
+
+  @override
+  void initState() {
+    super.initState();
+    _controllerCenter =
+        ConfettiController(duration: const Duration(seconds: 10));
+    _controllerCenterRight =
+        ConfettiController(duration: const Duration(seconds: 10));
+    _controllerCenterLeft =
+        ConfettiController(duration: const Duration(seconds: 10));
+    _controllerTopCenter =
+        ConfettiController(duration: const Duration(seconds: 10));
+    _controllerBottomCenter =
+        ConfettiController(duration: const Duration(seconds: 10));
+  }
+
+  @override
+  void dispose() {
+    _controllerCenter.dispose();
+    _controllerCenterRight.dispose();
+    _controllerCenterLeft.dispose();
+    _controllerTopCenter.dispose();
+    _controllerBottomCenter.dispose();
+    super.dispose();
+  }
+
+  /// A custom Path to paint stars.
+  Path drawStar(Size size) {
+    // Method to convert degree to radians
+    double degToRad(double deg) => deg * (pi / 180.0);
+
+    const numberOfPoints = 5;
+    final halfWidth = size.width / 2;
+    final externalRadius = halfWidth;
+    final internalRadius = halfWidth / 2.5;
+    final degreesPerStep = degToRad(360 / numberOfPoints);
+    final halfDegreesPerStep = degreesPerStep / 2;
+    final path = Path();
+    final fullAngle = degToRad(360);
+    path.moveTo(size.width, halfWidth);
+
+    for (double step = 0; step < fullAngle; step += degreesPerStep) {
+      path.lineTo(halfWidth + externalRadius * cos(step),
+          halfWidth + externalRadius * sin(step));
+      path.lineTo(halfWidth + internalRadius * cos(step + halfDegreesPerStep),
+          halfWidth + internalRadius * sin(step + halfDegreesPerStep));
+    }
+    path.close();
+    return path;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SafeArea(
+      child: Stack(
+        children: <Widget>[
+          //CENTER
+          Align(
+            alignment: Alignment.center,
+            child: ConfettiWidget(
+              confettiController: _controllerCenter,
+              blastDirectionality: BlastDirectionality
+                  .explosive, //confetti yönü random
+              shouldLoop:
+              true, //animasyon biter bitmez tekrar başlar
+              colors: const [
+                Colors.green,
+                Colors.blue,
+                Colors.pink,
+                Colors.orange,
+                Colors.purple
+              ], // kullanılacak renkleri el ile belirtme
+              createParticlePath: drawStar, //özel bir şekil / yol tanımlama.
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: TextButton(
+                onPressed: () {
+                  _controllerCenter.play();
+                },
+                child: _display('blast\nstars')),
+          ),
 
+          //CENTER RIGHT -- Emit left
+          Align(
+            alignment: Alignment.centerRight,
+            child: ConfettiWidget(
+              confettiController: _controllerCenterRight,
+              blastDirection: pi, // confetti yönü: sol
+              particleDrag: 0.05, //konfetiye sürükle uygula
+              emissionFrequency: 0.05, // ne sıklıkta yayması gerekir
+              numberOfParticles: 20, //  parçacık sayısı
+              gravity: 0.05, // yerçekimi veya tam hız
+              shouldLoop: false,
+              colors: const [
+                Colors.green,
+                Colors.blue,
+                Colors.pink
+              ], //kullanılacak renkleri el ile belirtme
+              strokeWidth: 1,
+              strokeColor: Colors.white,
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+                onPressed: () {
+                  _controllerCenterRight.play();
+                },
+                child: _display('pump left')),
+          ),
+
+          //CENTER LEFT - Emit right
+          Align(
+            alignment: Alignment.centerLeft,
+            child: ConfettiWidget(
+              confettiController: _controllerCenterLeft,
+              blastDirection: 0, // confetti yönü sağ
+              emissionFrequency: 0.6,
+              minimumSize: const Size(10,
+                  10), // konfeti için minimum potansiyel boyutu ayarlama (genişlik, yükseklik)
+              maximumSize: const Size(50,
+                  50), // konfeti için maksimum potansiyel boyutunu ayarlama (genişlik, yükseklik)
+              numberOfParticles: 1,
+              gravity: 0.1,
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: TextButton(
+                onPressed: () {
+                  _controllerCenterLeft.play();
+                },
+                child: _display('singles')),
+          ),
+
+          //TOP CENTER - shoot down
+          Align(
+            alignment: Alignment.topCenter,
+            child: ConfettiWidget(
+              confettiController: _controllerTopCenter,
+              blastDirection: pi / 2, //confetti yönü: yukarıya
+              maxBlastForce: 5, //  maksimum patlama kuvveti ayarlama
+              minBlastForce: 2, //  min patlama kuvveti ayarlama
+              emissionFrequency: 0.05,//parçacıkların yoğunluğu
+              numberOfParticles: 50, //aynı anda bir sürü parçacık
+              gravity: 1,
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: TextButton(
+                onPressed: () {
+                  _controllerTopCenter.play();
+                },
+                child: _display('goliath')),
+          ),
+          //BOTTOM CENTER
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: ConfettiWidget(
+              confettiController: _controllerBottomCenter,
+              blastDirection: -pi / 2,
+              emissionFrequency: 0.01,
+              numberOfParticles: 20,
+              maxBlastForce: 100,
+              minBlastForce: 80,
+              gravity: 0.3,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: TextButton(
+                onPressed: () {
+                  _controllerBottomCenter.play();
+                },
+                child: _display('hard and infrequent')),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Text _display(String text) {
+    return Text(
+      text,
+      style: const TextStyle(color: Colors.white, fontSize: 20),
     );
   }
 }
