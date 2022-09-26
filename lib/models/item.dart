fecb5a8bdae4ef4cf6cb36ed5e654cc5cae12276
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-26 21:50:53 +0300

    Aynı anda birden fazla paneli açabilme

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,7 +1,7 @@
 class Item {
   final String header;
   final String body;
-  bool? isExpanded;
+  bool isExpanded;
 
-  Item({required this.header,required this.body, this.isExpanded}) {}
+  Item({required this.header,required this.body, this.isExpanded=false}) {}
 }
--- lib/src/pages/multiple_expansion_panel.dart
+++ lib/src/pages/multiple_expansion_panel.dart
@@ -6,35 +6,40 @@ class MultipleExpansionPanelPage extends StatefulWidget {
   const MultipleExpansionPanelPage({Key? key}) : super(key: key);
 
   @override
-  State<MultipleExpansionPanelPage> createState() => _MultipleExpansionPanelPageState();
+  State<MultipleExpansionPanelPage> createState() =>
+      _MultipleExpansionPanelPageState();
 }
 
-class _MultipleExpansionPanelPageState extends State<MultipleExpansionPanelPage> {
-
+class _MultipleExpansionPanelPageState
+    extends State<MultipleExpansionPanelPage> {
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      title: const Text("Expansion Tile Widget"),
-    ),
-    body: SingleChildScrollView(
-      child: ExpansionPanelList.radio(
-        children: itemList
-            .map((item) => ExpansionPanelRadio(
-          //header'a tıklayınca da panelin açılıp kapanabilmesi için
-            canTapOnHeader: true,
-            headerBuilder: (context, isExpanded) => ListTile(
-              title: Text(
-                item.header,
-                style: const TextStyle(fontSize: 20),
-              ),
-            ),
-            body: ListTile(
-              title: Text(item.body, style: TextStyle(fontSize: 12)),
-            ),
-            value: item.header //todo: UNIQUE OLMALI!
-        ))
-            .toList(),
-      ),
-    ),
-  );
+        appBar: AppBar(
+          title: const Text("Expansion Tile Widget"),
+        ),
+        body: SingleChildScrollView(
+          child: ExpansionPanelList(
+            expansionCallback: (index, isExpanded) {
+              setState(() {
+                itemList[index].isExpanded = !isExpanded;
+              });
+            },
+            children: itemList
+                .map((item) => ExpansionPanel(
+                    //header'a tıklayınca da panelin açılıp kapanabilmesi için
+                    canTapOnHeader: true,
+                    headerBuilder: (context, isExpanded) => ListTile(
+                          title: Text(
+                            item.header,
+                            style: const TextStyle(fontSize: 20),
+                          ),
+                        ),
+                    body: ListTile(
+                      title: Text(item.body, style: TextStyle(fontSize: 12)),
+                    ),
+                    isExpanded: item.isExpanded))
+                .toList(),
+          ),
+        ),
+      );
 }
