52b09dc59cd2b322da3efa88e865cc93278f8c59
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-26 21:35:11 +0300

    Update expansion_panel.dart

--- lib/src/pages/expansion_panel.dart
+++ lib/src/pages/expansion_panel.dart
@@ -1,12 +1,37 @@
+import 'package:custom_widgets/constants/constants.dart';
+import 'package:custom_widgets/models/item.dart';
 import 'package:flutter/material.dart';
 
-class ExpansionPanelPage extends StatelessWidget {
+class ExpansionPanelPage extends StatefulWidget {
   const ExpansionPanelPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
+  State<ExpansionPanelPage> createState() => _ExpansionPanelPageState();
+}
 
-    );
-  }
+class _ExpansionPanelPageState extends State<ExpansionPanelPage> {
+  final List<Item> items = [
+    Item(header: 'Panel 1', body: Constants.loremIpsum)
+  ];
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Expansion Tile Widget"),
+        ),
+        body: SingleChildScrollView(
+          child: ExpansionPanelList.radio(
+            children: items
+                .map((item) => ExpansionPanelRadio(
+                    headerBuilder: (context, isExpanded) =>ListTile(
+                      title: Text(
+                        item.header,
+                        style:const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    body: ListTile(title: Text(item.body,style:TextStyle(fontSize: 12)),),
+                    value: item.header))
+                .toList(),
+          ),
+        ),
+      );
 }
