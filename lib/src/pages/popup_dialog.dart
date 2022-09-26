160a300f48fc9cd1b9297e843264b437d3f0f44a
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-09-26 21:24:19 +0300

    updated constants

--- lib/src/pages/popup_dialog.dart
+++ lib/src/pages/popup_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:custom_widgets/constants/constants.dart';
 import 'package:flutter/material.dart';
 
 class PopupDialogPage extends StatelessWidget {
@@ -24,8 +25,7 @@ class PopupDialogPage extends StatelessWidget {
                               child: const Text('OK'))
                         ],
                         title: const Text('My Title'),
-                        content: const Text(
-                            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                        content: const Text(Constants.loremIpsum),
                       ));
             },
           ),
